44be23b9d74444f246f606b5759014719b134154
Oliver Dantzer <oliverdantzer@gmail.com>
2025-02-10 19:09:11 -0500

    update Object object server field naming

--- src/worker_object_server/object_server.py
+++ src/worker_object_server/object_server.py
@@ -11,23 +11,23 @@ Indexable = Union[dict, list]
 class Object:
     position: Position
 
-    def __init__(self, db_api: ObjectServer, position: Position):
+    def __init__(self, object_server: ObjectServer, position: Position):
         self.position = position
-        self.db_api = db_api
+        self.object_server = object_server
 
     def __getitem__(self, name: str) -> Any:
-        value = self.db_api.get_at_position(self.position + name)
+        value = self.object_server.get_at_position(self.position + name)
         if value is Indexable:
-            return Object(self.db_api, self.position + name)
+            return Object(self.object_server, self.position + name)
         else:
             return value
 
     def __setitem__(self, name: str, value: Any) -> None:
-        value = self.db_api.get_at_position(self.position + name)
+        value = self.object_server.get_at_position(self.position + name)
         position = self.position + name
         update = Update(
             timestamp=datetime.now(), position=position, data=value)
-        self.db_api.add_update(update)
+        self.object_server.add_update(update)
 
 
 class ObjectServer:
@@ -35,7 +35,7 @@ class ObjectServer:
         self.data = {}
         self.server = UpdateServer(
             get_at_position=self.get_at_position, handle_incoming_update=self.handle_incoming_update)
-        self.root = Object(db_api=self, position=Position())
+        self.root = Object(object_server=self, position=Position())
 
     def get_at_position(self, position: Position) -> Any:
         current = self.data
