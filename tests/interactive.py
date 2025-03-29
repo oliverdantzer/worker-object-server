055a4cbf6a813b1ef2deca088e67f404ff245c3a
Oliver Dantzer <oliverdantzer@gmail.com>
2025-03-29 14:25:06 -0400

    load static config file into interactive test

--- tests/interactive.py
+++ tests/interactive.py
@@ -1,6 +1,10 @@
 from worker_object_server.object_server import ObjectServer
-obj = ObjectServer()
+import json
 
+with open('tests/worker_parameters_config.json', 'r') as f:
+    config = json.load(f)
+
+obj = ObjectServer(config)
 
 def end():
     obj.stop()
